0c8898513f76ff1ca548a10304c960fb793e5ddb
blopatka <bartlomiej.lopatka@gmail.com>
2017-11-05 20:54:52 +0100

    2017 i 2016

--- site/include/menu.php
+++ site/include/menu.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 if(!$add_site) exit;
 ?>
 <div id="menu">
@@ -7,12 +7,14 @@ if(!$add_site) exit;
 		<li><a href="index.php?go=lebenslauf" title="Lebenslauf">Lebenslauf</a></li>
 		<hr>
 		<li>Prace/Malerei:</li>
-		<li><a href="index.php?go=2010" title="2010">2010</a></li>
-		<li><a href="index.php?go=2011" title="2011">2011</a></li>
-		<li><a href="index.php?go=2012" title="2012">2012</a></li>
-		<li><a href="index.php?go=2013" title="2013">2013</a></li>
-		<li><a href="index.php?go=2014" title="2014">2014</a></li>
+		<li><a href="index.php?go=2017" title="2017">2017</a></li>
+		<li><a href="index.php?go=2016" title="2016">2016</a></li>
 		<li><a href="index.php?go=2015" title="2015">2015</a></li>
+		<li><a href="index.php?go=2014" title="2014">2014</a></li>
+		<li><a href="index.php?go=2013" title="2013">2013</a></li>
+		<li><a href="index.php?go=2012" title="2012">2012</a></li>
+		<li><a href="index.php?go=2011" title="2011">2011</a></li>
+		<li><a href="index.php?go=2010" title="2010">2010</a></li>		
 		<!--<li><a href="index.php?go=pejzaze" title="Pejzaże">PEJZAŻE</a></li>
 		<li><a href="index.php?go=fantazje" title="Fantazje">FANTAZJE</a></li>
 		<li><a href="index.php?go=miasta" title="Miasta">MIASTA</a></li>
@@ -23,7 +25,7 @@ if(!$add_site) exit;
 		<div class="zewnetrzneLinki">
 			<li>LINKI</li>
 			<li><a href="http://www.annaartgaleria.pl" title="Galeria Sztuki Wpółczesnej RYNEK 7" target="_blank"><img src="images/annaartgaleria.jpg"/></a></li>
-			<li><a href="http://www.galeria-jaroma.eu" title="Galeria ART Jaroma" target="_blank"><img style="width: 140px; height: auto;" src="images/galeriaartjaroma.png"/></a></li>
+
 		</div>
 	</ul>
 </div>
